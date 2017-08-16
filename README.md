8b1435240ab4f02fb16ad9af1acf4ffca11c4992
linlif <1264954361@qq.com>
2017-08-16 22:16:17 +0800

    提取css和js到独立的文件

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # douban_movies
 
-获取豆瓣电影top250 && 正在热映的电影，v1.0版本已经完成，后续会继续完善！
+抓取豆瓣网站的电影信息，效果请点击链接： https://linlif.github.io/douban_movies/
+
+>注意： 如果没有数据，请允许浏览器加载js脚本。
+
+![](https://ws1.sinaimg.cn/large/006tKfTcgy1fiiniav7fjj30ee0880tf.jpg)
---
+++ app.js
@@ -0,0 +1,194 @@
+$(function () {
+  var count = 6;
+  var $ul = $('#ul');
+  var $ul2 = $('#ul2');
+
+  // 渲染数据函数,返回一个DOM对象
+  function createLis(datas) {
+    var lis = '';
+    var json = datas;
+    var datas = json.subjects;
+//      console.log(json.start);
+    for (var i = 0; i < datas.length; i++) {
+      lis +=
+          '<a target="_blank" href="' + datas[i].alt + '"><li>' +
+          '<p class="topNum">Top.' + (json.start + i + 1) + '</p>' +
+          '<h1 class="title">' + datas[i].title + '</h1>' +
+          '<p class="original_title">' + datas[i].original_title + '</p>' +
+          '<div class="imgBox"><img src="' + datas[i].images.large + '"></div>' +
+          '<p class="average">评分：<b class="score">' + datas[i].rating.average + '</b> 分</p>' +
+          '<p class="genres">类型：' + datas[i].genres + '</p>' +
+          '<p class="directors">导演：' + datas[i].directors[0].name + '</p>' +
+          '<p class="year">年份：' + datas[i].year + '</p>' +
+          '</li></a>';
+    }
+    return lis;
+  }
+
+  // 院线热映
+  function showContent1() {
+    $.ajax({
+      type: "get", //jquey是不支持post方式跨域的
+      async: false,
+      data: {
+        count: count,
+        start: '0'
+      },
+//      url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
+      url: "https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b", //跨域请求的URL
+      dataType: "jsonp",
+      //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
+      jsonp: "callback",
+      //自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
+      jsonpCallback: "success_jsonpCallback",
+      //成功获取跨域服务器上的json数据后,会动态执行这个callback函数
+      success: function (json) {
+        var datas = json.collections;
+        var page = $('#page');
+        var aaa = '';
+
+        // 生成页码
+        for (var j = 0; j < Math.ceil(json.total / count); j++) {
+          aaa += '<a>' + (j + 1) + '</a>';
+        }
+        page.html(aaa);
+
+        // 渲染数据
+        var datas = json;
+        $ul.html(createLis(datas));
+
+        // 点击页码请求对应数据
+        var as = $('#page a');
+        // 动态添加激活状态类名
+        as.eq(0).addClass('active');
+        for (var k = 0; k < as.length; k++) {
+          (function (k) {
+            as[k].onclick = function () {
+//              this.setAttribute('class','active');
+              $(this).addClass('active').siblings().removeClass('active');
+              $.ajax({
+                type: "get", //jquey是不支持post方式跨域的
+                async: false,
+                data: {
+                  count: count,
+                  start: k * count
+                },
+//                url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
+                url: "https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b", //跨域请求的URL
+                dataType: "jsonp",
+                //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
+                jsonp: "callback",
+                //自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
+                jsonpCallback: "success_jsonpCallback",
+                //成功获取跨域服务器上的json数据后,会动态执行这个callback函数
+                success: function (json) {
+                  var datas = json;
+                  // 渲染数据
+                  $ul.html(createLis(datas));
+                }
+              });
+            }
+          })(k);
+//          k = 0;
+        } // end of for
+      }
+    });
+  }
+
+  // top250
+  function showContent2() {
+    $.ajax({
+      type: "get", //jquey是不支持post方式跨域的
+      async: false,
+      data: {
+        count: count,
+        start: '0'
+      },
+//      url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
+      url: "https://api.douban.com/v2/movie/top250?apikey=0b2bdeda43b5688921839c8ecb20399b", //跨域请求的URL
+      dataType: "jsonp",
+      //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
+      jsonp: "callback",
+      //自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
+      jsonpCallback: "success_jsonpCallback",
+      //成功获取跨域服务器上的json数据后,会动态执行这个callback函数
+      success: function (json) {
+        var datas = json.collections;
+        var page2 = $('#page2');
+        var aaa = '';
+
+        // 生成页码
+        for (var j = 0; j < Math.ceil(json.total / count); j++) {
+          aaa += '<a>' + (j + 1) + '</a>';
+        }
+        page2.html(aaa);
+
+        // 渲染数据
+        var datas = json;
+        $ul2.html(createLis(datas));
+
+        // 点击页码请求对应数据
+        var as = $('#page2 a');
+        // 动态添加激活状态类名
+        as.eq(0).addClass('active');
+        for (var k = 0; k < as.length; k++) {
+          (function (k) {
+            as[k].onclick = function () {
+//              this.setAttribute('class','active');
+              $(this).addClass('active').siblings().removeClass('active');
+              $.ajax({
+                type: "get", //jquey是不支持post方式跨域的
+                async: false,
+                data: {
+                  count: count,
+                  start: k * count
+                },
+//                url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
+                url: "https://api.douban.com/v2/movie/top250?apikey=0b2bdeda43b5688921839c8ecb20399b", //跨域请求的URL
+                dataType: "jsonp",
+                //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
+                jsonp: "callback",
+                //自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
+                jsonpCallback: "success_jsonpCallback",
+                //成功获取跨域服务器上的json数据后,会动态执行这个callback函数
+                success: function (json) {
+                  var datas = json;
+                  // 渲染数据
+                  $ul2.html(createLis(datas));
+                }
+              });
+            }
+          })(k);
+//          k = 0;
+        } // end of for
+      }
+    });
+  }
+
+  showContent1();
+//    showContent2();
+
+
+  // 顶部Tab切换
+  $('#myDiv h2').on('click', function () {
+    var index = $(this).index();
+    var tabcontent = $('.content >div');
+    var $ulLis = $('#ul').find('li').length;
+    var $ul2Lis = $('#ul2').find('li').length;
+
+//      console.log(index);
+    tabcontent.eq(index).css('display', 'block').siblings().css('display', 'none');
+    $(this).addClass('active').siblings().removeClass('active');
+
+    if (index === 0 && $ulLis === 0) {
+      showContent1();
+      console.log(index);
+    }
+
+    if (index === 1 && $ul2Lis === 0) {
+      showContent2();
+      console.log(index);
+    }
+  })
+
+});
