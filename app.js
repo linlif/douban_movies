744973f81d2a13eaef50106f1b18077fd201dc42
linlif <1264954361@qq.com>
2017-08-17 15:51:21 +0800

    响应式布局，移动端自适应

--- app.js
+++ app.js
@@ -23,20 +23,18 @@ $(function () {
           '<p class="year">年份：' + datas[i].year + '</p>' +
           '</li></a>';
     }
-    return lis;
+
+    return lis || "<div class='noResult'>啊呜...暂无结果<a href=''>返回首页</a></div>";
+
   }
 
-  // 院线热映
-  function showContent1() {
+
+  function showContent(params, url) {
     $.ajax({
       type: "get", //jquey是不支持post方式跨域的
       async: false,
-      data: {
-        count: count,
-        start: '0'
-      },
-//      url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
-      url: "https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b", //跨域请求的URL
+      data: params,
+      url: url, //跨域请求的URL
       dataType: "jsonp",
       //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
       jsonp: "callback",
@@ -44,7 +42,7 @@ $(function () {
       jsonpCallback: "success_jsonpCallback",
       //成功获取跨域服务器上的json数据后,会动态执行这个callback函数
       success: function (json) {
-        // var datas = json.collections;
+        var datas = json.collections || json.subjects;
         var page = $('#page');
         var aaa = '';
 
@@ -75,7 +73,7 @@ $(function () {
                   start: k * count
                 },
 //                url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
-                url: "https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b", //跨域请求的URL
+                url: url, //跨域请求的URL
                 dataType: "jsonp",
                 //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
                 jsonp: "callback",
@@ -97,74 +95,74 @@ $(function () {
   }
 
   // top250
-  function showContent2() {
-    $.ajax({
-      type: "get", //jquey是不支持post方式跨域的
-      async: false,
-      data: {
-        count: count,
-        start: '0'
-      },
-//      url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
-      url: "https://api.douban.com/v2/movie/top250?apikey=0b2bdeda43b5688921839c8ecb20399b", //跨域请求的URL
-      dataType: "jsonp",
-      //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
-      jsonp: "callback",
-      //自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
-      jsonpCallback: "success_jsonpCallback",
-      //成功获取跨域服务器上的json数据后,会动态执行这个callback函数
-      success: function (json) {
-        var datas = json.collections;
-        var page2 = $('#page2');
-        var aaa = '';
-
-        // 生成页码
-        for (var j = 0; j < Math.ceil(json.total / count); j++) {
-          aaa += '<a>' + (j + 1) + '</a>';
-        }
-        page2.html(aaa);
-
-        // 渲染数据
-        var datas = json;
-        $ul2.html(createLis(datas));
-
-        // 点击页码请求对应数据
-        var as = $('#page2 a');
-        // 动态添加激活状态类名
-        as.eq(0).addClass('active');
-        for (var k = 0; k < as.length; k++) {
-          (function (k) {
-            as[k].onclick = function () {
-//              this.setAttribute('class','active');
-              $(this).addClass('active').siblings().removeClass('active');
-              $.ajax({
-                type: "get", //jquey是不支持post方式跨域的
-                async: false,
-                data: {
-                  count: count,
-                  start: k * count
-                },
-//                url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
-                url: "https://api.douban.com/v2/movie/top250?apikey=0b2bdeda43b5688921839c8ecb20399b", //跨域请求的URL
-                dataType: "jsonp",
-                //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
-                jsonp: "callback",
-                //自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
-                jsonpCallback: "success_jsonpCallback",
-                //成功获取跨域服务器上的json数据后,会动态执行这个callback函数
-                success: function (json) {
-                  var datas = json;
-                  // 渲染数据
-                  $ul2.html(createLis(datas));
-                }
-              });
-            }
-          })(k);
-//          k = 0;
-        } // end of for
-      }
-    });
-  }
+//   function showContent2() {
+//     $.ajax({
+//       type: "get", //jquey是不支持post方式跨域的
+//       async: false,
+//       data: {
+//         count: count,
+//         start: '0'
+//       },
+// //      url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
+//       url: "https://api.douban.com/v2/movie/top250?apikey=0b2bdeda43b5688921839c8ecb20399b", //跨域请求的URL
+//       dataType: "jsonp",
+//       //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
+//       jsonp: "callback",
+//       //自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
+//       jsonpCallback: "success_jsonpCallback",
+//       //成功获取跨域服务器上的json数据后,会动态执行这个callback函数
+//       success: function (json) {
+//         var datas = json.collections;
+//         var page2 = $('#page2');
+//         var aaa = '';
+//
+//         // 生成页码
+//         for (var j = 0; j < Math.ceil(json.total / count); j++) {
+//           aaa += '<a>' + (j + 1) + '</a>';
+//         }
+//         page2.html(aaa);
+//
+//         // 渲染数据
+//         var datas = json;
+//         $ul2.html(createLis(datas));
+//
+//         // 点击页码请求对应数据
+//         var as = $('#page2 a');
+//         // 动态添加激活状态类名
+//         as.eq(0).addClass('active');
+//         for (var k = 0; k < as.length; k++) {
+//           (function (k) {
+//             as[k].onclick = function () {
+// //              this.setAttribute('class','active');
+//               $(this).addClass('active').siblings().removeClass('active');
+//               $.ajax({
+//                 type: "get", //jquey是不支持post方式跨域的
+//                 async: false,
+//                 data: {
+//                   count: count,
+//                   start: k * count
+//                 },
+// //                url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
+//                 url: "https://api.douban.com/v2/movie/top250?apikey=0b2bdeda43b5688921839c8ecb20399b", //跨域请求的URL
+//                 dataType: "jsonp",
+//                 //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
+//                 jsonp: "callback",
+//                 //自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
+//                 jsonpCallback: "success_jsonpCallback",
+//                 //成功获取跨域服务器上的json数据后,会动态执行这个callback函数
+//                 success: function (json) {
+//                   var datas = json;
+//                   // 渲染数据
+//                   $ul2.html(createLis(datas));
+//                 }
+//               });
+//             }
+//           })(k);
+// //          k = 0;
+//         } // end of for
+//       }
+//     });
+//   }
 
   // 搜索结果
   function searchResult(val) {
@@ -177,7 +175,7 @@ $(function () {
         start: '0'
       },
 //      url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
-      url: "https://api.douban.com/v2/movie/search?&q="+val, //跨域请求的URL "apikey=0b2bdeda43b5688921839c8ecb20399b"
+      url: "https://api.douban.com/v2/movie/search?&q=" + val, //跨域请求的URL "apikey=0b2bdeda43b5688921839c8ecb20399b"
       dataType: "jsonp",
       //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
       jsonp: "callback",
@@ -201,7 +199,7 @@ $(function () {
         // 渲染数据
         $ul3.html(createLis(datas));
         // 设置其他tab隐藏
-        $('.content >div').eq(2).css('display','block').siblings().css('display','none');
+        $('.content >div').eq(2).css('display', 'block').siblings().css('display', 'none');
         $('#myDiv').children().removeClass('active');
 
         // 点击页码请求对应数据
@@ -221,7 +219,7 @@ $(function () {
                   start: k * count
                 },
 //                url: "https://api.douban.com/v2/book/user/119280372/collections", //跨域请求的URL
-                url: "https://api.douban.com/v2/movie/search?&q="+val, //跨域请求的URL
+                url: "https://api.douban.com/v2/movie/search?&q=" + val, //跨域请求的URL
                 dataType: "jsonp",
                 //传递给请求处理程序，用以获得jsonp回调函数名的参数名(默认为:callback)
                 jsonp: "callback",
@@ -247,40 +245,61 @@ $(function () {
   $('.search').on('keyup', function (event) {
     var query = $('.search').val();
     if (event.keyCode === 13) {
-      searchResult(query);
+      var params = {count: count, start: '0', q: query};
+      var url = 'https://api.douban.com/v2/movie/search?';
+      showContent(params, url);
     }
   });
 
   // 搜索按钮搜索
-  $('.submit').on('click',function () {
+  $('.submit').on('click', function () {
     var query = $('.search').val();
-    searchResult(query);
+    var params = {count: count, start: '0', q: query};
+    var url = 'https://api.douban.com/v2/movie/search?';
+    showContent(params, url);
   });
 
-  showContent1();
+  // 院线热映
+  $('.in_theaters').on('click', function () {
+    var params = {count: count, start: '0'};
+    var url = 'https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b';
+    showContent(params, url);
+    $(this).addClass('active').siblings().removeClass('active');
+  })
+
+  // top 250
+  $('.top250').on('click', function () {
+    var params = {count: count, start: '0'};
+    var url = 'https://api.douban.com/v2/movie/top250?apikey=0b2bdeda43b5688921839c8ecb20399b';
+    showContent(params, url);
+    $(this).addClass('active').siblings().removeClass('active');
+  })
+
+  $('.in_theaters').trigger('click')
+
 //    showContent2();
 
 
   // 顶部Tab切换
-  $('#myDiv h2').on('click', function () {
-    var index = $(this).index();
-    var tabcontent = $('.content >div');
-    var $ulLis = $('#ul').find('li').length;
-    var $ul2Lis = $('#ul2').find('li').length;
-
-//      console.log(index);
-    tabcontent.eq(index).css('display', 'block').siblings().css('display', 'none');
-    $(this).addClass('active').siblings().removeClass('active');
-
-    if (index === 0 && $ulLis === 0) {
-      showContent1();
-      console.log(index);
-    }
-
-    if (index === 1 && $ul2Lis === 0) {
-      showContent2();
-      console.log(index);
-    }
-  })
+//   $('#myDiv h2').on('click', function () {
+//     var index = $(this).index();
+//     var tabcontent = $('.content >div');
+//     var $ulLis = $('#ul').find('li').length;
+//     var $ul2Lis = $('#ul2').find('li').length;
+//
+// //      console.log(index);
+//     tabcontent.eq(index).css('display', 'block').siblings().css('display', 'none');
+//     $(this).addClass('active').siblings().removeClass('active');
+//
+//     if (index === 0 && $ulLis === 0) {
+//       showContent1();
+//       console.log(index);
+//     }
+//
+//     if (index === 1 && $ul2Lis === 0) {
+//       showContent2();
+//       console.log(index);
+//     }
+//   })
 
 });
